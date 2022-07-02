74b566f692a43117976fca73f4300e2c7335da7a
kaiyeej <58097285+kaiyeej@users.noreply.github.com>
2022-07-02 09:06:05 +0800

    Update env.php

--- core/env.php
+++ core/env.php
@@ -3,7 +3,13 @@
 define('BASE_URL', 'http://localhost/trisakay/');
 define("BASE_PATH", __DIR__ . "/../");
 
-define("HOST", "localhost");
+/*define("HOST", "localhost");
 define("USER", "root");
 define("PASSWORD", "");
-define("DBNAME", "tri_sakay_db");
+define("DBNAME", "tri_sakay_db");*/
+
+define("HOST", "localhost");
+define("USER", "u981310152_root");
+define("PASSWORD", "triSakay123!");
+define("DBNAME", "u981310152_trisakay");
+
