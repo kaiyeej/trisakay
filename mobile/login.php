fdc7b802f0d1e4342c0d2c9e506b544811acc6ee
Ginx <songalingginery@gmail.com>
2022-09-21 09:11:58 +0800

    fix login with idtoken

--- mobile/login.php
+++ mobile/login.php
@@ -19,16 +19,21 @@ if (isset($username) && isset($password)) {
     if ($count_users['category'] == 'U') {
         $getUser = $mysqli_connect->query("SELECT * FROM tbl_users WHERE user_id='$count_users[user_id]'");
         $data = $getUser->fetch_array();
-        $mysqli_connect->query("UPDATE `tbl_users` SET `id_token`='$id_token' WHERE `user_id`='$count_users[user_id]'");
-        $response["user_fname"] = $data['user_fname'];
-        $response["user_lname"] = $data['user_lname'];
-        $response["contact_number"] = $data['contact_number'];
-        $response["category"] = $data['category'];
-        $response["username"] = $data['username'];
-        $response["user_id"] = $data['user_id'];
-        $response["response"] = 1;
+        $result_id_token = $mysqli_connect->query("UPDATE `tbl_users` SET `id_token`='$id_token' WHERE `user_id`='$count_users[user_id]'");
+        if ($result_id_token) {
+            $response["user_fname"] = $data['user_fname'];
+            $response["user_lname"] = $data['user_lname'];
+            $response["contact_number"] = $data['contact_number'];
+            $response["category"] = $data['category'];
+            $response["username"] = $data['username'];
+            $response["user_id"] = $data['user_id'];
+            $response["response"] = 1;
+        } else {
+            $response["response"] = 0;
+        }
+    } else {
+        $response["response"] = -1;
     }
-    // $response["response"] = -1;
 }
 array_push($response_array['array_data'], $response);
 echo json_encode($response_array);
