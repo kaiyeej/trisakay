3332e1f5cb2168f94b91988e53e2cd87cc8e3d11
Ginx <songalingginery@gmail.com>
2022-12-05 16:54:44 +0800

    Update getDriversData.php

--- mobile/getDriversData.php
+++ mobile/getDriversData.php
@@ -13,6 +13,7 @@ $response_array['array_data'] = array();
 $fetch = $mysqli_connect->query("SELECT * FROM tbl_users WHERE category='D' AND latitude != 0 AND longitude != 0 AND `status`='0'");
 while ($row = $fetch->fetch_array()) {
 	$response = array();
+	$distance = getDistance($row['latitude'],  $row['longitude'], getUserLocation($user_id)['latitude'], getUserLocation($user_id)['longitude'], 0.250);
 	$response["user_id"] = $row['user_id'];
 	$response['fullname'] = $row['user_fname'] . " " . $row['user_lname'];
 	$response["ratings"] = getRatingFunc($row['user_id']);
@@ -20,6 +21,7 @@ while ($row = $fetch->fetch_array()) {
 	$response["latitude"] = $row['latitude'];
 	$response["longitude"] = $row['longitude'];
 	$response["session_id"] = $user_id;
+	$response["distance_status"] = $distance;
 	$response["status"] = getTransactionStatus($user_id, $row['user_id']);
 
 
