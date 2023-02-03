d4718221dff975ae35d8a016a0639b22023209b8
Kaye Jacildo <58097285+kaiyeej@users.noreply.github.com>
2023-02-03 14:17:11 +0800

    Update index.php

--- index.php
+++ index.php
@@ -44,21 +44,21 @@ if (!isset($_SESSION["trisakay_user_id"])) {
           <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
             <span class="mdi mdi-menu"></span>
           </button>
-          <div class="search-field d-none d-md-block">
-            <form class="d-flex align-items-center h-100" action="#">
-              <div class="input-group">
-                <div class="input-group-prepend bg-transparent">
-                  <i class="input-group-text border-0 mdi mdi-magnify"></i>
-                </div>
-                <input type="text" class="form-control bg-transparent border-0" placeholder="Search Transaction">
-              </div>
-            </form>
-          </div>
+          <!--<div class="search-field d-none d-md-block">-->
+          <!--  <form class="d-flex align-items-center h-100" action="#">-->
+          <!--    <div class="input-group">-->
+          <!--      <div class="input-group-prepend bg-transparent">-->
+          <!--        <i class="input-group-text border-0 mdi mdi-magnify"></i>-->
+          <!--      </div>-->
+          <!--      <input type="text" class="form-control bg-transparent border-0" placeholder="Search Transaction">-->
+          <!--    </div>-->
+          <!--  </form>-->
+          <!--</div>-->
           <ul class="navbar-nav navbar-nav-right">
             
             <li class="nav-item d-none d-lg-block full-screen-link">
               <a class="nav-link">
-                <i class="mdi mdi-fullscreen" id="fullscreen-button"></i>
+               
               </a>
             </li>
             <li class="nav-item nav-logout d-none d-lg-block">
@@ -115,11 +115,6 @@ if (!isset($_SESSION["trisakay_user_id"])) {
   <script type="text/javascript">
     var modal_detail_status = 0;
     $(document).ready(function() {
-        $(".select2").select2();
-
-        $(".select2").css({
-          "width": "100%"
-        });
     });
 
     function logout() {
