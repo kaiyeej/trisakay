94c0e566260ad3a5c79644464f4d077728cd4451
Kaye Jacildo <58097285+kaiyeej@users.noreply.github.com>
2023-02-08 17:35:14 +0800

    Update env.php

--- core/env.php
+++ core/env.php
@@ -4,12 +4,12 @@ define('BASE_URL', 'https://juancoder.com/trisakay/');
 // define('BASE_URL', 'trisakay/');
 define("BASE_PATH", __DIR__ . "/../");
 
-define("HOST", "localhost");
-define("USER", "root");
-define("PASSWORD", "");
-define("DBNAME", "tri_sakay_db");
-
 // define("HOST", "localhost");
-// define("USER", "u981310152_trisakay_root");
-// define("PASSWORD", "triSakay123!");
-// define("DBNAME", "u981310152_trisakay");
+// define("USER", "root");
+// define("PASSWORD", "");
+// define("DBNAME", "tri_sakay_db");
+
+define("HOST", "localhost");
+define("USER", "u981310152_trisakay_root");
+define("PASSWORD", "triSakay123!");
+define("DBNAME", "u981310152_trisakay");
