d9302b4f8a9c91a5976cbdd7704178722322f4ac
ginery <songalingginery@gmail.com>
2023-02-26 14:11:39 +0800

    added arrive pop up

--- core_mobile/my_functions.php
+++ core_mobile/my_functions.php
@@ -60,7 +60,7 @@ function sendNotif($user_id, $title, $body)
 	//$body = "Test";
 
 	//Creating the notification array.
-	$notification = array('title' => $title, 'text' => $body);
+	$notification = array('title' => $title, 'body' => $body);
 
 	//This array contains, the token and the notification. The 'to' attribute stores the token.
 	$arrayToSend = array('registration_ids' => $tokens, 'notification' => $notification, 'priority' => 'high');
