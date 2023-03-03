ef996320fa6f32a957079496ce776e44d81df681
ginery <songalingginery@gmail.com>
2023-03-03 16:53:39 +0800

    Update getTransactionHistory.php

--- mobile/getTransactionHistory.php
+++ mobile/getTransactionHistory.php
@@ -22,6 +22,7 @@ while ($data = $fetch_users->fetch_array()) {
     $response['status'] = $data['status'];
     $response['remarks'] = $data['remarks'];
     $response['driver_id'] = $data['driver_id'];
+    $response['fuel_consumption'] = $data['fuel_consumption'];
     $response['date_added'] =  date('F j, Y', strtotime($data['date_added']));
     $response['rating_status'] = checkDriverRating($data['transaction_id']);
     $response['rating_remarks'] = getRatingRemarks($data['transaction_id']);
