a5dac2a7baab3a2bc6ab9be132440e676b79ca8e
ginery <songalingginery@gmail.com>
2023-03-04 15:18:28 +0800

    added user img

--- mobile/getUserDetails.php
+++ mobile/getUserDetails.php
@@ -21,6 +21,7 @@ $response["user_id"] = $data['user_id'];
 $response["user_fname"] = $data['user_fname'];
 $response["user_mname"] = $data['user_mname'];
 $response["user_lname"] = $data['user_lname'];
+$response["user_img"] = $data['user_img'];
 $response["category"] = $data['category'];
 $response["username"] = $data['username'];
 $response["license_number"] = $data['license_number'];
